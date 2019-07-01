c972954d2ba1795bfb51f65ef1ac55b49a4bc6c7
Tash Tenner <tashfonetikas@gmail.com>
2019-07-01 13:32:43 +0200

    add German words to 4 lessons

--- js/word.js
+++ js/word.js
@@ -29,6 +29,26 @@ let ella = new Word("ella");
 let está = new Word("está");
 let hola = new Word("hola");
 
+// German words
+// lesson 1
+let ich = new Word("ich");
+let bin = new Word("bin");
+let hier = new Word("hier");
+let dort = new Word("dort");
+let du = new Word("du");
+let bist = new Word("bist");
+let und = new Word("und");
+let auch = new Word("auch");
+let aber = new Word("aber");
+let ja = new Word("ja");
+let nein = new Word("nein");
+let oder = new Word("oder");
+let er = new Word("er");
+let sie = new Word("sie");
+let ist = new Word("ist");
+let hallo = new Word("hallo");
+
+// Spanish words
 // lesson 2
 let me = new Word("me");
 let llamo = new Word("llamo");
@@ -44,7 +64,7 @@ let austria = new Word("austria");
 let españa = new Word("españa");
 let francia = new Word("francia");
 let inglaterra = new Word("inglaterra");
-let portugal = new Word("portugal");
+let portugalEs = new Word("portugalEs");
 let qué = new Word("qué");
 let país = new Word("país");
 let este = new Word("este");
@@ -56,6 +76,30 @@ let dónde = new Word("dónde");
 let encantado = new Word("encantado");
 let encantada = new Word("encantada");
 
+// German words
+// lesson 2
+let heiße = new Word("heiße");
+let heißt = new Word("heißt");
+let heißt = new Word("heißt");
+let wie = new Word("wie");
+let das = new Word("das");
+let deutschland = new Word("deutschland");
+let österreich = new Word("österreich");
+let spanien = new Word("spanien");
+let frankreich = new Word("frankreich");
+let englandDt = new Word("englandDt");
+let portugalDt = new Word("portugalDt");
+let welches = new Word("welches");
+let land = new Word("land");
+let komme = new Word("komme");
+let kommst = new Word("kommst");
+let kommt = new Word("kommt");
+let aus = new Word("aus");
+let woher = new Word("woher");
+let freut = new Word("freut");
+let mich = new Word("mich");
+
+// Spanish words
 // lesson 3
 let el = new Word("el");
 let la = new Word("la");
@@ -67,6 +111,20 @@ let tienes = new Word("tienes");
 let tiene = new Word("tiene");
 // missing German words: "den", "wo"
 
+// German words
+// lesson 3
+let der = new Word("der");
+let die = new Word("die");
+let apfel = new Word("apfel");
+let plátano = new Word("plátano");
+let brot = new Word("brot");
+let habe = new Word("habe");
+let hast = new Word("hast");
+let hat = new Word("hat");
+let den = new Word("den");
+let wo = new Word("wo");
+
+// Spanish words
 // lesson 4
 let rojo = new Word("rojo");
 let roja = new Word("roja");
@@ -84,8 +142,21 @@ let color = new Word("color");
 // De qué color es ... - lógica alemana: Qué color tiene ...
 
 // German words
-let ich = new Word("ich");
-let nein = new Word("nein");
+// lesson 4
+let rot = new Word("rojo");
+let grün = new Word("grün");
+let gelb = new Word("gelb");
+let braun = new Word("braun");
+let weiß = new Word("weiß");
+// faltan los sufijos de los colores
+let ein = new Word("ein");
+let eine = new Word("eine");
+let einen = new Word("einen");
+let kein = new Word("kein");
+let keine = new Word("keine");
+let keinen = new Word("keinen");
+let farbe = new Word("farbe");
+// De qué color es ... - lógica alemana: Qué color tiene ...
 
 /* if base Spanish and target language German:
 load object spanishGerman 
@@ -142,8 +213,6 @@ Framework of a language:
 ... > Indoeuropean Languages > Latin Languages > Spanish, French, Italian, etc
 
 Framework of Spanish - structure of the Spanish language: const Spanish = {}
-
-change strings in const Spanish to already existing objects
 
 >>1.1. parts of speech - all come from class WORD
 >>1.1.6.1.3.3. Third-person neuter/inanimate singular = does not exist
